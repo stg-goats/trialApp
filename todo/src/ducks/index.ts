553108332adf97db1cb555d23493eda107e4de5a
stg-goats <>
2020-06-02 07:08:50 +0900

    Merge branch 'master' of https://github.com/stg-goats/trialApp
    # Conflicts:
    #	todo/package.json
    #	todo/public/index.html
    #	todo/public/manifest.json
    #	todo/src/components/App.tsx
    #	todo/src/components/button/index.tsx
    #	todo/src/index.tsx
    #	todo/yarn.lock

--- todo/src/ducks/index.ts
+++ todo/src/ducks/index.ts
@@ -1,27 +1,4 @@
 import { combineReducers, createStore } from 'redux'
+import text from './text'
 
-// Actions
-const TEXTCHANGE = 'TEXTCHANGE'
-
-const textChange = ({ value }) => {
-  return {
-    type: TEXTCHANGE,
-    value,
-  }
-}
-
-// Reducer
-const initialState = { textvalue: 'default text' }
-
-const textReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case TEXTCHANGE:
-      return { ...state, ...action.value }
-    default:
-      return state
-  }
-}
-
-const reducers = combineReducers(textReducer)
-
-export default createStore(reducers)
+export default createStore(combineReducers({ text }))
